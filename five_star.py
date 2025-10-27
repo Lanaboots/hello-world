06ce95a1b5d0a1d9e23decbf22a79fb60fae5a20
Alanna <88062073+Lanaboots@users.noreply.github.com>
2025-10-27 17:06:45 -0500

    Create a Five Star Rating System for "Al's Pizza."

--- five_star.py
+++ five_star.py
@@ -1,26 +1,15 @@
 # Write code below 💖 
 # imported code from codedex to work on. 
 
-rating = float 0.0
+rating = float(input("How many stars would you like to give Al's Pizza?"))
 
-print("How many stars would you like to give Al's Pizza?")
-print("1 star")
-print("2 stars")
-print("3 stars")
-print("4 stars")
-print("5 stars")
-
-star = int(input("Enter how many stars will you give Al's Pizza:"))
-
-if star > 4.5:
-  print("Extrodinary!")
-elif star > 4:
+if rating > 4.5:
+  print("Extraodinary!")
+elif rating > 4:
   print("Excellent!")
-elif star > 3:
+elif rating > 3:
   print("Good.")
-elif star > 2:
+elif rating > 2:
   print("Fair.")
 else:
   print("Poor.")
-
-print("rating:", rating)

cdd57603534253b3295234b60807355ae13ced8f
Alanna <88062073+Lanaboots@users.noreply.github.com>
2025-10-27 17:42:11 -0500

    Implement fun fact generator with answer function
    Corrected the function call to 'answer' and fixed a typo in 'randint'.

---
+++ snapple.py
@@ -0,0 +1,18 @@
+import random  #perhaps change print to answer and that will correct the problem I am having with the code. 
+
+fact = random.randit(0, 5)
+
+if fact == 0:
+  answer("Flamingos turn pink from eating shrimp.")
+elif fact == 1:
+  answer("The only food that doesnt\'t spoil is honey.")
+elif fact == 2:
+  answer("Shrimp can only swim backwards.")
+elif fact == 3:
+  answer("A taste bud\'slife span is about 10 days.")
+elif fact == 4:
+  answer("It is impossible to sneeze while sleeping.")
+elif fact == 5:
+  answer("It is illegal to sing off-key in North Carolina.")
+else:
+  print("Fun Fact", + answer)

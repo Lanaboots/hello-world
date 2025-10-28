ac490a6f75eafdb571b8902d2fb54657d78d8968
Alanna <88062073+Lanaboots@users.noreply.github.com>
2025-10-28 09:29:38 -0500

    Replace answer function with print statements

--- snapple.py
+++ snapple.py
@@ -3,16 +3,16 @@ import random  #perhaps change print to answer and that will correct the problem
 fact = random.randit(0, 5)
 
 if fact == 0:
-  answer("Flamingos turn pink from eating shrimp.")
+  print("Flamingos turn pink from eating shrimp.")
 elif fact == 1:
-  answer("The only food that doesnt\'t spoil is honey.")
+  print("The only food that doesnt\'t spoil is honey.")
 elif fact == 2:
-  answer("Shrimp can only swim backwards.")
+  print("Shrimp can only swim backwards.")
 elif fact == 3:
-  answer("A taste bud\'slife span is about 10 days.")
+  print("A taste bud\'slife span is about 10 days.")
 elif fact == 4:
-  answer("It is impossible to sneeze while sleeping.")
+  print("It is impossible to sneeze while sleeping.")
 elif fact == 5:
-  answer("It is illegal to sing off-key in North Carolina.")
+  print("It is illegal to sing off-key in North Carolina.")
 else:
   print("Fun Fact", + answer)

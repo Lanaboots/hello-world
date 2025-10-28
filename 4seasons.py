a43602bb56c399753d144354a568239ae2ac86da
Alanna <88062073+Lanaboots@users.noreply.github.com>
2025-10-28 09:55:03 -0500

    Correct month conditions for season output
    Fix conditional checks for month input.

---
+++ 4seasons.py
@@ -0,0 +1,13 @@
+
+month = int(input("Enter the month here:"))
+
+if month == 1 or == 2 == 3:
+  print("Winter")
+elif month == 4 or == 5 or == 6:
+  print("Spring")
+elif month == 7 or == 8 or == 9:
+  print("Summer")
+elif month ==10 or 11 or == 12:
+  print("Autumn")
+else:
+  print("Invalid") 
